184ca15c6c67329208549aa67d9e2e6c4a27aa38
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-11-02 02:35:20 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    !BRANDS.include?(brand) ? BRANDS << brand : nil
+    # !BRANDS.include?(brand) ? BRANDS << brand : nil
   end
 
   def cobble
